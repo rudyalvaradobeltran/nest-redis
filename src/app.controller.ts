9c7d04fadc6020b1d9629151fbbe2bd5768b83fa
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-05-12 22:25:30 -0400

    Cache get, set, del and reset

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,59 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Inject, CACHE_MANAGER } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { Profile } from './common/models/Profile';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  fakeValue = 'My name is Juan';
+  fakeModel: Profile = {
+    name: 'Juan',
+    email: 'juan@gmail.com',
+  };
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  @Get('getCacheString')
+  async getSimpleString() {
+    const value = await this.cacheManager.get('my-string');
+    if (value) {
+      return {
+        data: value,
+        loadsFrom: 'redis cache',
+      };
+    }
+    await this.cacheManager.set('my-string', this.fakeValue, { ttl: 300 });
+    return {
+      data: this.fakeValue,
+      loadFrom: 'fake data',
+    };
+  }
+
+  @Get('getObject')
+  async getObject() {
+    const profile = await this.cacheManager.get<Profile>('my-profile');
+    if (profile) {
+      return {
+        data: profile,
+        loadsFrom: 'redis cache',
+      };
+    }
+    await this.cacheManager.set<Profile>('my-profile', this.fakeModel, {
+      ttl: 300,
+    });
+    return {
+      data: this.fakeModel,
+      loadFrom: 'fake model',
+    };
+  }
+
+  @Get('delete')
+  async deleteCache() {
+    await this.cacheManager.del('my-string');
+    await this.cacheManager.del('my-profile');
+  }
+
+  @Get('reset')
+  async resetCache() {
+    await this.cacheManager.reset();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module, CacheModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import * as redisStore from 'cache-manager-redis-store';
 @Module({
   imports: [
     CacheModule.register({
       store: redisStore,
       host: 'localhost',
-      port: 5003,
+      port: 6379,
     }),
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
